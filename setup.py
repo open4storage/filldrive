aa62378f0932c3009adc6392909e553a216520dc
Julio <julio.colon@emc.com>
2017-03-23 10:59:59 -0400

    Added setup.exe

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def read(relative):
+    """
+    Read file contents and return a list of lines.
+    ie, read the VERSION file
+    """
+    contents = open(relative, 'r').read()
+    return [l for l in contents.split('\n') if l != '']
+
+
+setup(name='filldrive',
+      version='1.0',
+      description='This program creates a number of file with certain size automatically',
+      author='Dell | EMC',
+      install_requires=read('./requirements.txt'),
+      url='https://github.com/open4storage/filldrive'
+     )
